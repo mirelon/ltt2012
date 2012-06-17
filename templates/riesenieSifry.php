799d61eda2d573bbfe4e28a63bd325e6a9416c39
mirelon <miso@github.ksp.sk>
2012-06-17 02:51:35 +0200

    ajax loader

--- templates/riesenieSifry.php
+++ templates/riesenieSifry.php
@@ -1,4 +1,5 @@
 <?php
+  sleep(10);
   if(isset($_POST['nazov']) && isset($_POST['riesenie'])) {
     if($_POST['nazov'] == 'trojita' && strtolower($_POST['riesenie']) == 'soschudak') {
       echo 'ok';
--- templates/sifry.php
+++ templates/sifry.php
@@ -21,19 +21,27 @@ text-align:center;
 <form id="riesenieSifry" method="post" action="">
 <input type="text" id="riesenie" />
 <input type="submit" id="odosli" value="Odošli riešenie" /><br/>
-<span id="result"></span>
+<div id="result"></div>
 </form>
 
 <script type="text/javascript">
 $(function(){
   
+  function random(n) {
+    return Math.floor((Math.random()*n)+1);
+  }
+
   function odosliRiesenie(nazov, riesenie) {
-    var obj = {
-      'nazov':nazov,
-      'riesenie':riesenie,
+    $('#result').html('Overujeme...<img id="loader" src="images/ajax-loader.gif" alt="Overujeme..."/>').fadeIn();
+    $('#loader').css({'position':'relative'}).animate({'left':'500'}, 4000);
+    for(var i=0;i<20;i++) {
+      var duration = 200 + random(2000);
+      var x = random(300);
+      var y = -random(260);
+      $('#loader').animate({'left':x, 'top':y}, duration);
     }
-    $.post('?page=riesenieSifry', obj, function(response){
-        console.log(response);
+    $.post('?page=riesenieSifry', {'nazov':nazov, 'riesenie':riesenie}, function(response){
+        $('#loader').clearQueue();
         if($.trim(response) == 'ok') {
           $('#result').text('Správne').css({'color': 'green'}).fadeIn();
         } else {
